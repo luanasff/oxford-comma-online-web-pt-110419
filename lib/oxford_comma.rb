9536df0058646852ac903388a0a52acca63a8390
Luana Faria Lane <luanasfff@gmail.com>
2019-11-13 21:47:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
+ new_array=array.join 
+ new_array=array.join(" and ")
 
+return array.join(' and ') if array.size < 3
+  array[-1] = "and " + array[-1]
+  array.join(', ')
 end
+
+
